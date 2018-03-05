ba3cec809076557d9b4203847e694c082ae53376
FU-9 <racezhao@163.com>
2018-03-05 20:58:05 +0800

    线程池与进程池

--- 并发编程/19线程池与进程池.py
+++ 并发编程/19线程池与进程池.py
@@ -9,10 +9,14 @@ if __name__ == '__main__':
     pool = ProcessPoolExecutor(4)
     # for i in range(10):
     #     pool.submit(task)
-    pool.map(task,range(10))
+    pool.map(task,(1,2,3,4))
 
     pool.shutdown(wait=True)#关闭池入口 相当于pool.close()+pool.join()
     print("主")
 
+#submit 异步提交任务
+#result 取得结果
+#shutdown(wait=True)#关闭池入口 相当于pool.close()+pool.join()
+#add_done_callback(fn)回调函数
 
 #线程池同进程池
